aa5b8bfdd10d6144cb4a4e746d48104c82e3b465
Viktoria <hello@viktoriamf.com>
2021-06-23 20:28:41 +0200

    add fancy console logs for assignment

--- src/index.tsx
+++ src/index.tsx
@@ -12,10 +12,11 @@ ReactDOM.render(
 );
 
 // fancy console log
+console.warn('Beware of fancy console log incoming!');
 let styles: string[] = [
   'background-image: url(https://images.pexels.com/photos/373465/pexels-photo-373465.jpeg?cs=srgb&dl=pexels-leah-kelley-373465.jpg&fm=jpg)',
   'background-size: cover',
-  'color: #fff',
+  'color: #000',
   'padding: 10px 20px',
   'line-height: 35px',
   'width : 70px',
@@ -23,6 +24,8 @@ let styles: string[] = [
   'border : 5px solid black',
 ];
 console.log('%c Have fun!', styles.join(';'));
+console.warn('The message above might not be working in Firefox.');
+console.log('Beware of unicorns 🦄');
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
