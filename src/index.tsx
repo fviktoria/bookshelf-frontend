9dda046d71dc0ac3165f76e61e8684739a1479a4
Viktoria <hello@viktoriamf.com>
2021-06-23 11:06:33 +0200

    feat(notification): add notification

--- src/index.tsx
+++ src/index.tsx
@@ -33,6 +33,27 @@ const requestNotificationPermission = () => {
 
 requestNotificationPermission();
 
+// show fake notification
+const img =
+  'https://images.pexels.com/photos/373465/pexels-photo-373465.jpeg?cs=srgb&dl=pexels-leah-kelley-373465.jpg&fm=jpg';
+const text = 'Your friend gerti345 requested to borrow a book!';
+const title = 'Borrow Request';
+const options = {
+  body: text,
+  icon: './assets/images/bookshelf-icon.png',
+  vibrate: [200, 100, 200],
+  tag: 'borrow-request',
+  image: img,
+  badge: 'https://spyna.it/icons/android-icon-192x192.png',
+  actions: [{ action: 'Detail', title: 'View', icon: 'https://via.placeholder.com/128/ff0000' }],
+};
+
+navigator.serviceWorker.ready.then(function (serviceWorker) {
+  setTimeout(() => {
+    serviceWorker.showNotification(title, options);
+  }, 5);
+});
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
