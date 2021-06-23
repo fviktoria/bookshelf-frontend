eb90859bfd3be9a6a56c6b2b38c9685ff517e567
Viktoria <hello@viktoriamf.com>
2021-06-23 11:18:26 +0200

    feat(notifications): show notification

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -1,6 +1,8 @@
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
+import { showDummyNotification } from './util/dummy-notification';
+
 // This lets the app load faster on subsequent visits in production, and gives
 // it offline capabilities. However, it also means that developers (and users)
 // will only see deployed updates on subsequent visits to a page, after all the
@@ -15,7 +17,7 @@ const isLocalhost = Boolean(
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
     // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
 );
 
 type Config = {
@@ -43,11 +45,14 @@ export function register(config?: Config) {
 
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
-        navigator.serviceWorker.ready.then(() => {
+        navigator.serviceWorker.ready.then((serviceWorker) => {
           console.log(
             'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://cra.link/PWA'
+              'worker. To learn more, visit https://cra.link/PWA',
           );
+
+          console.log('try showing dummy notification');
+          showDummyNotification(serviceWorker);
         });
       } else {
         // Is not localhost. Just register service worker
@@ -74,7 +79,7 @@ function registerValidSW(swUrl: string, config?: Config) {
               // content until all client tabs are closed.
               console.log(
                 'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://cra.link/PWA.'
+                  'tabs for this page are closed. See https://cra.link/PWA.',
               );
 
               // Execute callback
@@ -109,10 +114,7 @@ function checkValidServiceWorker(swUrl: string, config?: Config) {
     .then((response) => {
       // Ensure service worker exists, and that we really are getting a JS file.
       const contentType = response.headers.get('content-type');
-      if (
-        response.status === 404 ||
-        (contentType != null && contentType.indexOf('javascript') === -1)
-      ) {
+      if (response.status === 404 || (contentType != null && contentType.indexOf('javascript') === -1)) {
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker.ready.then((registration) => {
           registration.unregister().then(() => {
---
+++ src/util/dummy-notification.ts
@@ -0,0 +1,20 @@
+const img =
+  'https://images.pexels.com/photos/373465/pexels-photo-373465.jpeg?cs=srgb&dl=pexels-leah-kelley-373465.jpg&fm=jpg';
+const text = 'Your friend gerti345 requested to borrow a book!';
+const title = 'Borrow Request';
+const options = {
+  body: text,
+  icon: './assets/images/bookshelf-icon.png',
+  vibrate: [200, 100, 200],
+  tag: 'borrow-request',
+  image: img,
+  badge: 'https://spyna.it/icons/android-icon-192x192.png',
+  actions: [{ action: 'Detail', title: 'View', icon: 'https://via.placeholder.com/128/ff0000' }],
+};
+
+export const showDummyNotification = (serviceWorker: ServiceWorkerRegistration) => {
+  setTimeout(() => {
+    alert('timeout over');
+    serviceWorker.showNotification(title, options);
+  }, 5);
+};
