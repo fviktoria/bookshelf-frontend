aca611fef14d44dee966a4aa2aa75663ec45a9e5
Viktoria <hello@viktoriamf.com>
2021-06-23 10:10:17 +0200

    add console log

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,17 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
+let styles: string[] = [ 
+  "background-image: url(https://ux.christmas//static/teaser-1-95bcc16151dc1d561bd79ede903e9e7c.jpg)", 
+  "background-size: cover", 
+  "color: #fff", 
+  "padding: 10px 20px", 
+  "line-height: 35px", 
+  "width : 70px", 
+  "height : 70px", 
+  "border : 5px solid black"]
+console.log("%c Have fun!", styles.join(";") );
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
